22ad1eea78ed66c33ac576c71bc2d9b95e260bdf
gabriel barros <gabrielbarrosba@gmail.com>
2022-11-27 22:08:20 -0300

    up

--- src/main/java/publi/social/springboot2/controller/ParcController.java
+++ src/main/java/publi/social/springboot2/controller/ParcController.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import java.util.List;
 @RequestMapping("parceiros")
 @Log4j2
 @RequiredArgsConstructor
+@CrossOrigin
 public class ParcController {
     private final DateUtil dateUtil;
     private final ParcService parcService;
